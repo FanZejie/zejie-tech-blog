bc0d88a188b77568226da5c95d42289bfad4b198
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-07-10 18:05:55 +0800

    change docs to blogs

--- src/app/source.ts
+++ src/app/source.ts
@@ -3,7 +3,7 @@ import { createMDXSource } from 'next-docs-mdx';
 import { loader } from 'next-docs-zeta/source';
 
 export const { getPage, getPages, pageTree } = loader({
-  baseUrl: '/docs',
-  rootDir: 'docs',
+  baseUrl: '/blogs',
+  rootDir: 'blogs',
   source: createMDXSource(map),
 });
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,17 +5,17 @@ export default function Header() {
     <header className="supports-backdrop-blur fixed left-0 right-0 top-0 z-40 bg-white/75 py-4 backdrop-blur dark:bg-dark/75">
       <div className="mx-auto flex max-w-4xl items-center justify-between px-3 xl:max-w-5xl xl:px-0">
         <div>
-          <Link href={"/home"} className="text-2xl">
+          <Link href={"/"} className="text-2xl">
             {"zejie's Tech Blog"}
           </Link>
         </div>
         <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
           <Link
-            href="/post"
+            href="/blogs"
             className="hidden font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400
               sm:block"
           >
-            post
+            Blogs
           </Link>
 
           <Link

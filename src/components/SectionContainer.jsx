265d5158219f99ae5b5c671c8ba768f7b993c082
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-07-10 21:41:55 +0800

    填充高度

--- src/components/SectionContainer.jsx
+++ src/components/SectionContainer.jsx
@@ -1,6 +1,6 @@
 export default function SectionContainer({ children }) {
     return (
-      <section className=" max-w-full  sm:px-6 dark:bg-gray-900">{children}</section>
+      <section className=" h-full max-w-full  sm:px-6 dark:bg-gray-900">{children}</section>
     )
   }
   

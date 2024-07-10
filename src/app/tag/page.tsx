25529d9ab59c9977ab60d5fb16ff46b2726f91b0
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-07-11 00:22:30 +0800

    remove tags query

--- src/app/tag/page.tsx
+++ src/app/tag/page.tsx
@@ -1,6 +1,6 @@
-import { getAllTags } from "@/actions/actions"
+
 export default async function TagPage() {
-    const tags =await getAllTags()
+   
     return (
           <div className="flex flex-col items-center justify-center min-h-screen py-2">
              <h1 className="text-6xl font-bold">Tag Page</h1>

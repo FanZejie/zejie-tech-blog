17b63d923f25aff89c9961986733d795e3fc169a
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-07-15 11:48:29 +0800

    调整黑暗主题颜色

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,13 +3,13 @@ import BlogCard from "@/components/BlogCard"
 const Blogs = () => {
     return (
         <div className="flex flex-row  w-full mt-5 h-full">
-            <div className=" w-1/5 bg-gray-100 rounded-lg" >
+            <div className=" w-1/5 bg-gray-100 rounded-lg dark:bg-gray-800" >
             <div className="text-4xl font-bold uppercase text-cyan-500">All Tags</div>
                 <ul className="mt-2">
-                    <li className="px-4 py-2 text-sm font-medium uppercase text-gray-500">Nextjs(3)</li>
-                    <li className="px-4 py-2 text-sm font-medium uppercase text-gray-500">TypeScript(3)</li>
-                    <li className="px-4 py-2 text-sm font-medium uppercase text-gray-500">JavaScript(3)</li>
-                    <li className="px-4 py-2 text-sm font-medium uppercase text-gray-500">AWS(4)</li>
+                    <li className="px-4 py-2 text-sm font-medium uppercase text-gray-500 dark:text-gray-100">Nextjs(3)</li>
+                    <li className="px-4 py-2 text-sm font-medium uppercase text-gray-500 dark:text-gray-100">TypeScript(3)</li>
+                    <li className="px-4 py-2 text-sm font-medium uppercase text-gray-500 dark:text-gray-100">JavaScript(3)</li>
+                    <li className="px-4 py-2 text-sm font-medium uppercase text-gray-500 dark:text-gray-100">AWS(4)</li>
                 </ul>
             </div>
             <div className=" w-2/3 ml-8" >
